57ecb39892a83b222889bbdf1b14fc4351415cd6
David Ono <david-ono10@outlook.com>
2024-01-01 21:26:21 -0300

    feat: add new pages for list and edit foods.

--- expiration-date-of-food/src/app/foods/[slug]/page.tsx
+++ expiration-date-of-food/src/app/foods/[slug]/page.tsx
@@ -3,8 +3,7 @@ import { Flex, Text, Button } from '@radix-ui/themes';
 export default function Home() {
   return (
     <Flex direction="column" gap="2">
-      <Text>Menu Principal</Text>
-      <Text>Lista de Alimentos</Text>
+      <Text>Adicionar e ou Editar</Text>
     </Flex>
   );
 }
--- expiration-date-of-food/src/app/foods/page.tsx
+++ expiration-date-of-food/src/app/foods/page.tsx
@@ -3,7 +3,6 @@ import { Flex, Text, Button } from '@radix-ui/themes';
 export default function Home() {
   return (
     <Flex direction="column" gap="2">
-      <Text>Menu Principal</Text>
       <Text>Lista de Alimentos</Text>
     </Flex>
   );
