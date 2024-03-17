ff01671b04455dc0c4fffa6f8d52358ad0e8542f
David Ono <david-ono10@outlook.com>
2024-03-16 21:22:23 -0300

    fix: remove unnecessary pages

--- expiration-date-of-food/src/app/foods/page.tsx
+++
@@ -1,9 +0,0 @@
-import { Flex, Text, Button } from '@radix-ui/themes';
-
-export default function Home() {
-  return (
-    <Flex direction="column" gap="2">
-      <Text>Lista de Alimentos</Text>
-    </Flex>
-  );
-}
