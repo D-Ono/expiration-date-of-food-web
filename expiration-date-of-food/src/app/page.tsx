590b8f33ecba1f3c2d4eff5ab0690e052698bbde
David Ono <david-ono10@outlook.com>
2024-03-17 23:10:38 -0300

    feat: add examples of componentes

--- expiration-date-of-food/src/app/page.tsx
+++ expiration-date-of-food/src/app/page.tsx
@@ -3,7 +3,7 @@ import { Dialog, DialogClose, DialogContent, DialogFooter, DialogHeader, DialogT
 
 export default function Home() {
   return (
-    <div className="bg-slate-900 p-8 px-28 h-screen">
+    <div className="bg-gray-900 p-8 px-28 h-full">
       <div className="flex justify-between mb-8">
         <h1 className="text-white text-3xl">Lista de Alimentos</h1>
        <Dialog>
@@ -24,19 +24,135 @@ export default function Home() {
         </DialogContent>
        </Dialog>
       </div>
-      <div className="flex white border-2 rounded	p-4">
-        <div>
-          <h1 className=" text-xl text-emerald-300	">Cheetos</h1>
-          <p className="text-white text-xs"><strong>Categoria:</strong> Salgadinhos</p>
-          <p className="text-white text-xs"><strong>Descrição: </strong>Está localizado na comoda da Copa</p>
-          <p className="text-white text-xs"><strong>Quantidade:</strong> 04</p>
-          <p className="text-white text-xs"><strong>Data de vencimento:</strong> 18/02/2024</p>
-          <div className="p-2 border-color border-emerald-300 border-2">
-            <p className="text-white text-xs">Pouco Urgente</p>
+      <div className="flex rounded justify-between	p-4 px-6 bg-gray-800">
+        <div className="flex-row gap-8">
+          <h1 className=" text-xl text-slate-100">Cheetos</h1>
+          <p className="text-slate-300 text-xs py-2"><strong>Descrição: </strong>Está localizado na comoda da Copa</p>
+          <p className="text-slate-300 text-xs"><strong>Categoria:</strong> Salgadinhos</p>
+          <p className="text-slate-300 text-xs py-2"><strong>Quantidade:</strong> 04</p>
+          <p className="text-slate-300 text-xs"><strong>Data de vencimento:</strong> 18/02/2024</p>
+          <div className="p-2 border-color rounded border-red-400 border-2 my-2 w-fit">
+            <p className="text-red-400 text-xs">Muito Urgente</p>
           </div>
+        </div> 
+        <div className="flex">
+          <div className="mr-2">
+            <Button>Editar</Button>
+          </div>
+          <Button>Excluir</Button>
         </div>
-        <div>
-          <Button>Editar</Button>
+      </div>
+      <div className="flex rounded justify-between	p-4 px-6 bg-gray-800 mt-6">
+        <div className="flex-row gap-8">
+          <h1 className=" text-xl text-slate-100">Cheetos</h1>
+          <p className="text-slate-300 text-xs py-2"><strong>Descrição: </strong>Está localizado na comoda da Copa</p>
+          <p className="text-slate-300 text-xs"><strong>Categoria:</strong> Salgadinhos</p>
+          <p className="text-slate-300 text-xs py-2"><strong>Quantidade:</strong> 04</p>
+          <p className="text-slate-300 text-xs"><strong>Data de vencimento:</strong> 18/02/2024</p>
+          <div className="p-2 border-color rounded border-amber-300 border-2 my-2 w-fit">
+            <p className="text-amber-300 text-xs">Fica de Olho</p>
+          </div>
+        </div> 
+        <div className="flex">
+          <div className="mr-2">
+            <Button>Editar</Button>
+          </div>
+          <Button>Excluir</Button>
+        </div>
+      </div>
+      <div className="flex rounded justify-between	p-4 px-6 bg-gray-800 mt-6">
+        <div className="flex-row gap-8">
+          <h1 className=" text-xl text-slate-100">Cheetos</h1>
+          <p className="text-slate-300 text-xs py-2"><strong>Descrição: </strong>Está localizado na comoda da Copa</p>
+          <p className="text-slate-300 text-xs"><strong>Categoria:</strong> Salgadinhos</p>
+          <p className="text-slate-300 text-xs py-2"><strong>Quantidade:</strong> 04</p>
+          <p className="text-slate-300 text-xs"><strong>Data de vencimento:</strong> 18/02/2024</p>
+          <div className="p-2 border-color rounded border-emerald-300 border-2 my-2 w-fit">
+            <p className="text-emerald-300 text-xs">Pouco Urgente</p>
+          </div>
+        </div> 
+        <div className="flex">
+          <div className="mr-2">
+            <Button>Editar</Button>
+          </div>
+          <Button>Excluir</Button>
+        </div>
+      </div>
+      <div className="flex rounded justify-between	p-4 px-6 bg-gray-800 mt-6">
+        <div className="flex-row gap-8">
+          <h1 className=" text-xl text-slate-100">Cheetos</h1>
+          <p className="text-slate-300 text-sm py-2">Está localizado na comoda da Copa</p>
+          <div className="flex gap-4">
+            <div className="p-2 border-color rounded border-red-400 border-2 my-2 w-fit">
+              <p className="text-red-400 text-xs">Categoria: Salgadinhos</p>
+            </div>
+            <div className="p-2 border-color rounded border-red-400 border-2 my-2 w-fit">
+              <p className="text-red-400 text-xs">Quantidade: 04</p>
+            </div>
+            <div className="p-2 border-color rounded border-red-400 border-2 my-2 w-fit">
+              <p className="text-red-400 text-xs">Data de vencimento: 18/02/2024</p>
+            </div>
+            <div className="p-2 border-color rounded border-red-400 border-2 my-2 w-fit">
+              <p className="text-red-400 text-xs">Pouco Urgente</p>
+            </div>
+          </div>
+        </div> 
+        <div className="flex">
+          <div className="mr-2">
+            <Button>Editar</Button>
+          </div>
+          <Button>Excluir</Button>
+        </div>
+      </div>
+      <div className="flex rounded justify-between	p-4 px-6 bg-gray-800 mt-6">
+        <div className="flex-row gap-8">
+          <h1 className=" text-xl text-slate-100">Cheetos</h1>
+          <p className="text-slate-300 text-sm py-2">Está localizado na comoda da Copa</p>
+          <div className="flex gap-4">
+            <div className="p-2 border-color rounded border-amber-300 border-2 my-2 w-fit">
+              <p className="text-amber-300 text-xs">Categoria: Salgadinhos</p>
+            </div>
+            <div className="p-2 border-color rounded border-amber-300 border-2 my-2 w-fit">
+              <p className="text-amber-300 text-xs">Quantidade: 04</p>
+            </div>
+            <div className="p-2 border-color rounded border-amber-300 border-2 my-2 w-fit">
+              <p className="text-amber-300 text-xs">Data de vencimento: 18/02/2024</p>
+            </div>
+            <div className="p-2 border-color rounded border-amber-300 border-2 my-2 w-fit">
+              <p className="text-amber-300 text-xs">Pouco Urgente</p>
+            </div>
+          </div>
+        </div> 
+        <div className="flex">
+          <div className="mr-2">
+            <Button>Editar</Button>
+          </div>
+          <Button>Excluir</Button>
+        </div>
+      </div>
+      <div className="flex rounded justify-between	p-4 px-6 bg-gray-800 mt-6">
+        <div className="flex-row gap-8">
+          <h1 className=" text-xl text-slate-100">Cheetos</h1>
+          <p className="text-slate-300 text-sm py-2">Está localizado na comoda da Copa</p>
+          <div className="flex gap-4">
+            <div className="p-2 border-color rounded border-emerald-300 border-2 my-2 w-fit">
+              <p className="text-emerald-300 text-xs">Categoria: Salgadinhos</p>
+            </div>
+            <div className="p-2 border-color rounded border-emerald-300 border-2 my-2 w-fit">
+              <p className="text-emerald-300 text-xs">Quantidade: 04</p>
+            </div>
+            <div className="p-2 border-color rounded border-emerald-300 border-2 my-2 w-fit">
+              <p className="text-emerald-300 text-xs">Data de vencimento: 18/02/2024</p>
+            </div>
+            <div className="p-2 border-color rounded border-emerald-300 border-2 my-2 w-fit">
+              <p className="text-emerald-300 text-xs">Pouco Urgente</p>
+            </div>
+          </div>
+        </div> 
+        <div className="flex">
+          <div className="mr-2">
+            <Button>Editar</Button>
+          </div>
           <Button>Excluir</Button>
         </div>
       </div>
