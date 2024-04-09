ef4bdaae180f7abba8417a5d0f2319ef1ccc5014
David Ono <david-ono10@outlook.com>
2024-04-08 21:56:49 -0300

    feat: add nav bar

--- expiration-date-of-food/src/app/page.tsx
+++ expiration-date-of-food/src/app/page.tsx
@@ -1,10 +1,21 @@
-import { CreateFood } from "@/components/form/create-food-form";
-import { Button } from "@/components/ui/button";
+import { NavigationMenu, NavigationMenuItem, NavigationMenuLink, NavigationMenuList } from "@/components/ui/navigation-menu";
+import Link from "next/link";
 
 export default function Home() {
   return (
     <div className="bg-gray-900 p-8 px-28 h-full">
         <h1 className="text-white text-3xl">Lista de Alimentos</h1>
+        <NavigationMenu>
+          <NavigationMenuList>
+            <NavigationMenuItem>
+              <Link href="/docs" legacyBehavior passHref>
+                <NavigationMenuLink>
+                  Documentation
+                </NavigationMenuLink>
+              </Link>
+          </NavigationMenuItem>
+          </NavigationMenuList>
+        </NavigationMenu>
     </div>
   );
 }
